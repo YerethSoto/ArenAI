56b9eb6d0924f3a18ede79a1fdc43ac102eadc8a
Yereth <87624491+yereth0@users.noreply.github.com>
2025-10-20 19:57:26 -0600

    Update ArenModel.py

--- AI/ArenModel.py
+++ AI/ArenModel.py
@@ -1,1 +1,15 @@
-import mindspore as ms
+import ollama
+
+response = ollama.chat(
+    model='deepseek-r1:latest',
+    
+    messages=[
+
+        {'role': 'user', 'content': 'What is the capital of china'}
+     
+
+
+    ],
+)
+
+print(response['message'] ['content'])
